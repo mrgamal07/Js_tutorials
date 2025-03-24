46e419f41a2074efc78fffc2fc26b51d440916a3
mrgamal07 <gamalarun@gmail.com>
2025-03-24 12:08:45 +0545

    init

---
+++ day1.js
@@ -0,0 +1,26 @@
+document.getElementById('btn').onclick= function(){
+    document.getElementById('demo') .innerHTML = new Date();
+}
+document.getElementById('bot').onclick=function(){
+    document.getElementById('momo').innerHTML="hello i am arun gamal form ravibhawan kathmandu"
+}
+
+document.getElementById('lgt').onclick=function(){
+    document.getElementById('myImage').src='pic_bulbon.gif';
+};
+document.getElementById('off').onclick=function(){
+    document.getElementById('myImage').src='pic_bulboff.gif';
+};
+
+
+ onclick=function myfunction(){
+    document.getElementById('mm').innerHTML="paragraph change.";
+    document.getElementById('day2').innerHTML="arun gamal!!";
+}
+document.getElementById('by').onclick=function(){
+    document.write(5+5);
+    window.alert(7*7);
+    alert(5*5);
+    console.log(100+100);
+    window.print();
+}
---
+++ day2.js
@@ -0,0 +1,26 @@
+function myfunctionn(){
+    document.getElementById('day1').innerHTML="gamal!!";
+}
+ document.getElementById('six').onclick=function(){
+    document.getElementById('six').innerHTML="6";
+ }
+    var x;
+    x=5;
+    var y=5;
+    var z=x+y;
+    document.getElementById('add').innerHTML="the value of z is:"+z;
+    
+    const price1=5;
+    const price2=10;
+    let total=price1+price2;
+    document.getElementById("six").innerHTML="the total is:"+total;
+
+    const pi=3.14;
+    let person="arun gamal";
+    let answer="yes i am";
+    document.getElementById('var').innerHTML= "the pi is:" + pi+"<br>" + person +"<br>" +answer;
+
+
+    let Name="arun"+ " " + "shrestha";
+     elm=document.getElementById('concatenated');
+     elm.innerHTML= "the name was:"+Name;
