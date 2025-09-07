da899f24caa2e253f7fd015b843197ea93278b73
mrgamal07 <gamalarun@gmail.com>
2025-09-07 22:36:24 +0545

    init

---
+++ 100daycoding/day7.js
@@ -0,0 +1,12 @@
+let text = "please locate where 'locate' occurs!";
+let index = text.indexOf("locate");
+ console.log(index);
+
+
+ let abc = "arungamal";
+ const myArr = abc.split("");
+  abc = "";
+  for(let i  = 0; i<myArr.length; i++){
+    abc +=myArr[i] +"\n";
+  }
+console.log(abc)
