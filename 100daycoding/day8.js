65fd7ebfb91b7d13db35a0e50af6949a21c54220
mrgamal07 <gamalarun@gmail.com>
2025-09-11 22:41:35 +0545

    init

---
+++ 100daycoding/day8.js
@@ -0,0 +1,10 @@
+let x = 123;
+console.log(String(x) + "<br>" +
+String (123));
+console.log( String(100+23));
+console.log(5+null);
+console.log("50" / "100");
+console.log("50" + "100");
+console.log(40 + "100");
+
+
