b550519e1d210f04c5751d82aeeed799f12b1a4c
mrgamal07 <gamalarun@gmail.com>
2025-09-12 23:00:09 +0545

    init

---
+++ 100daycoding/day9.js
@@ -0,0 +1,10 @@
+console.log(Math.floor(Math.random()*100));
+
+console.log(Math.floor(Math.random()*10));
+console.log(Math.floor(Math.random()*10))+9;
+
+function getRndInterger(min,max){
+    console.log(Math.floor(Math.random()*(max-min +1))+min);
+
+}
+getRndInterger(1,4);
