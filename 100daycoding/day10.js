4909ea99c3c217401e921704bb8759c26222efa6
mrgamal07 <gamalarun@gmail.com>
2025-09-13 23:29:24 +0545

    init

---
+++ 100daycoding/day10.js
@@ -0,0 +1,15 @@
+myfunction();
+function myfunction(){
+    let carName = "arun";
+    console.log(typeof carName + " " + carName);
+    console.log(carName);
+}
+
+let x = 10;
+console.log(x==5);
+console.log(x==10)
+console.log(x=="10")
+console.log(x==="10")
+
+const d = new Date("2025");
+console.log(d);
